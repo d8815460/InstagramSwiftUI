c07b17dbc150f247af21e0443dcc10a64d8485c7
JOBS <jobs@seektopser.com>
2023-02-23 09:58:27 +0800

    Initial Commit

---
+++ InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUIApp.swift
+//  InstagramSwiftUI
+//
+//  Created by Alex on 2023/2/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
